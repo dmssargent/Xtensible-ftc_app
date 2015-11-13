3282875c0545dd2dfc7bb1dcfb19f8157ff413e3
David Sargent <dmssargent@yahoo.com>
2015-11-12 22:28:01 -0600

    Manually commit problem files, fix forward declaration issues

--- Open CV/src/main/java/org/opencv/features2d/DescriptorExtractor.java
+++ Open CV/src/main/java/org/opencv/features2d/DescriptorExtractor.java
@@ -14,6 +14,8 @@ import java.util.List;
 // C++: class javaDescriptorExtractor
 //javadoc: javaDescriptorExtractor
 public class DescriptorExtractor {
+    private static final int
+            OPPONENTEXTRACTOR = 1000;
     public static final int
             SIFT = 1,
             SURF = 2,
@@ -29,8 +31,7 @@ public class DescriptorExtractor {
             OPPONENT_BRISK = OPPONENTEXTRACTOR + BRISK,
             OPPONENT_FREAK = OPPONENTEXTRACTOR + FREAK,
             OPPONENT_AKAZE = OPPONENTEXTRACTOR + AKAZE;
-    private static final int
-            OPPONENTEXTRACTOR = 1000;
+
     protected final long nativeObj;
 
 
