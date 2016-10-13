a3d044229fa1e0cb5908dd43b9fc301d87743b7e
David Sargent <dmssargent@yahoo.com>
2016-10-13 17:20:56 -0500

    Sensor things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDrive.java
@@ -2,14 +2,19 @@ package org.firstinspires.ftc.teamcode;
 
 
 import com.google.common.io.Files;
+import com.qualcomm.hardware.adafruit.AdafruitBNO055IMU;
+import com.qualcomm.hardware.adafruit.AdafruitI2cColorSensor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.teamcode.test.AdafruitSensorWrapper;
 import org.ftccommunity.ftcxtensible.robot.RobotContext;
 import org.ftccommunity.ftcxtensible.xsimplify.SimpleOpMode;
 
@@ -26,9 +31,11 @@ public class TestDrive extends SimpleOpMode {
     private DcMotor rightRear;
 
     private ColorSensor colorLeft;
-    private ColorSensor colorRight;
-
+    private AdafruitSensorWrapper colorRight;
+    private AdafruitL3GD20H gyro;
     private VuforiaLocalizer vuforia;
+    private Servo buttonLeft;
+    private Servo buttonRight;
 
     @Override
     public void init(RobotContext ctx) throws Exception {
@@ -36,15 +43,22 @@ public class TestDrive extends SimpleOpMode {
         leftRear = hardwareMap.dcMotor("leftRear");
         rightFront = hardwareMap.dcMotor("rightFront");
         rightRear = hardwareMap.dcMotor("rightRear");
-
+        buttonLeft = hardwareMap.servos().get("buttonLeft");
+        buttonRight = hardwareMap.servos().get("buttonRight");
+       // AdafruitI2cColorSensor
+        colorRight = new AdafruitSensorWrapper(hardwareMap.colorSensors().get("colorRight"), hardwareMap.deviceInterfaceModules().get("dim"), 0, false);
+        colorRight.beginColorSample(25);
         leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
         leftRear.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        VuforiaLocalizer.Parameters vuforiaParams = new VuforiaLocalizer.Parameters(robotControllerView().getId());
+        gyro = new AdafruitL3GD20H(new I2cDeviceSynchImpl(hardwareMap.i2cDevices().get("gyro"), false), false);
+        gyro.doInitialize();
+
+       VuforiaLocalizer.Parameters vuforiaParams = new VuforiaLocalizer.Parameters(robotControllerView().getId());
         vuforiaParams.vuforiaLicenseKey = Files.toString(new File("/sdcard/robot/vuforia.key"), Charset.forName("utf-8"));
         vuforiaParams.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
 
-        vuforia = ClassFactory.createVuforiaLocalizer(vuforiaParams);
+       // vuforia = ClassFactory.createVuforiaLocalizer(vuforiaParams);
         VuforiaTrackables vuforiaTrackables = vuforia.loadTrackablesFromAsset("FTC_2016-17");
 
         // Set names of trackables
@@ -52,14 +66,11 @@ public class TestDrive extends SimpleOpMode {
         vuforiaTrackables.get(1).setName("Tools");
         vuforiaTrackables.get(2).setName("Legos");
         vuforiaTrackables.get(3).setName("Gears");
-
-        //
     }
 
     @Override
     public void loop(RobotContext ctx) throws Exception {
 
-        // todO: fix direction of sideways
         final double robotAngle = gamepad1.leftJoystick.polar().getTheta() - Math.PI / 4;
         double rightY = gamepad1.rightJoystick.X();
         //final double r = coordinates.getR();
@@ -73,6 +84,20 @@ public class TestDrive extends SimpleOpMode {
         rightFront.setPower(v2);
         leftRear.setPower(v3);
         rightRear.setPower(v4);
+
+        if (gamepad1.isXPressed())
+            buttonLeft.setPosition(1);
+        if (gamepad1.isYPressed())
+            buttonLeft.setPosition(0);
+        if (gamepad1.isAPressed())
+            buttonRight.setPosition(0);
+        if (gamepad1.isBPressed())
+            buttonRight.setPosition(1);
+
+        telemetry.data("COLOR", colorRight.redOrBlue().toString());
+        telemetry.data("GYRO_X", gyro.readX());
+        telemetry.data("GYRO_Y", gyro.readY());
+        telemetry.data("GYRO_Z", gyro.readZ());
     }
 
     @Override
